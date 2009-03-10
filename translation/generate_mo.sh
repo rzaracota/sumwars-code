bd216b60dd5388fc1a0d74babf0662bdd0d56dad
kemic <>
2009-03-10 17:24:01 +0000

    uebersetzungen bearbeitet

--- translation/generate_mo.sh
+++ translation/generate_mo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # German
-msgfmt -o de/LC_MESSAGES/sumwars.mo de/LC_MESSAGES/sumwars.po
-msgfmt -o de/LC_MESSAGES/event.mo de/LC_MESSAGES/event.po
+msgfmt -c -v -o de/LC_MESSAGES/sumwars_sumwars.mo sumwars_sumwars_de.po
+msgfmt -c -v -o de/LC_MESSAGES/sumwars_events.mo sumwars_events_de.po
 
 # English
-msgfmt -o en/LC_MESSAGES/sumwars.mo en/LC_MESSAGES/sumwars.po
+#msgfmt -c -v -o en/LC_MESSAGES/sumwars_sumwars.mo sumwars_sumwars_en.po
+#msgfmt -c -v -o en/LC_MESSAGES/sumwars_events.mo sumwars_events_en.po
 
