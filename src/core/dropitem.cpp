3305a695cefeb9640d3f15bba33eb9e010b499e9
wuha <>
2009-03-12 10:54:49 +0000

    item dropp ani verbessert (items liegen nicht mehr im boden)

--- src/core/dropitem.cpp
+++ src/core/dropitem.cpp
@@ -4,12 +4,17 @@ DropItem::DropItem()
 {
 	m_angle_z = Random::random()*3.14159*2;
 
-	m_height = 2.0;
-	m_speed = -0.1;
+	float hadd = 0.1;
+	float acc = -20;
+	float v0 =4;
+	m_height = 2.0 +hadd;
 	
-	float t = -m_speed/0.001 + sqrt(m_speed*m_speed/(0.001*0.001) + m_height*2/0.001);
-	m_angle_x = t/50;
 	
+	float t = -v0/(2*acc)+sqrt(v0*v0/(4*acc*acc) - 2/acc);
+	m_speed = 4/t;
+	m_angle_x = 4;
+	
+	m_time =0;
 }
 
 void DropItem::toString(CharConv* cv)
@@ -49,18 +54,21 @@ std::string DropItem::getNameId()
 void DropItem::update(float time)
 {
 	m_time += time;
-	
-	if (m_height>0)
+	float hadd = 0.1;
+			
+	if (m_height>hadd)
 	{
-		m_speed += time/1000;
-		m_angle_x -= time/50;
-		m_height -= m_speed;
+		float acc = -20;
+		float v0 =4;
+		m_height =hadd+ 2 + v0*m_time/1000 + acc*m_time*m_time/1000000;
+		m_angle_x -= m_speed*time/1000;
 		
-		if (m_height < 0)
+		if (m_height < hadd)
 		{
-			m_height =0;
+			m_height =hadd;
 			m_angle_x=0;
 		}
+		DEBUG5("speed %f height %f angle %f time %f",m_speed,m_height,m_angle_x,m_time); 
 	}
 	
 }
