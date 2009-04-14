ae705d384b33afecf0c654abc108a65e14aa32d0
kemic <>
2009-04-14 14:19:51 +0000

    uebersetzung aktualisiert

--- translation/generate_mo.sh
+++ translation/generate_mo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # German
+echo "+++++++++++++ sumwars +++++++++++++"
 msgfmt -c -v -o de/LC_MESSAGES/sumwars_sumwars.mo sumwars_sumwars_de.po
+echo "+++++++++++++ xml +++++++++++++"
 msgfmt -c -v -o de/LC_MESSAGES/sumwars_xml.mo sumwars_xml_de.po
+echo "+++++++++++++ events +++++++++++++"
 msgfmt -c -v -o de/LC_MESSAGES/sumwars_events.mo sumwars_events_de.po
 
 # English
