0aaf5c7f0a01615e679fdd2be887320bb221404f
kalimgard <>
2009-04-23 09:46:17 +0000

    Wegpunkte auf tatsaechliche Masse angepasst

--- src/core/waypoint.cpp
+++ src/core/waypoint.cpp
@@ -10,11 +10,11 @@ Waypoint::Waypoint( int id) : FixedObject( id)
 bool Waypoint::init ()
 {
 	FixedObject::init();
-	
-	getShape()->m_type = Shape::CIRCLE;
-	getShape()->m_radius = 0.5;
+
+	getShape()->m_type = Shape::RECT;
+	getShape()->m_extent = Vector(2.4,2.2);
 	setLayer(LAYER_BASE | LAYER_AIR);
-	
+
 	setState(STATE_ACTIVE,false);
 	setSubtype("waypoint");
 	return true;
@@ -27,14 +27,14 @@ bool  Waypoint::update (float time)
 		// alle Spieler im Umkreis von 12m suchen
 		WorldObjectMap *players = getRegion()->getPlayers();
 		WorldObjectMap::iterator it;
-		
+
 		Shape s;
 		s.m_type = Shape::CIRCLE;
 		s.m_center = getShape()->m_center;
 		s.m_radius = 4;
-		
+
 		Player* pl;
-		
+
 		for (it = players->begin(); it != players->end(); ++it)
 		{
 			pl = dynamic_cast<Player*>(it->second);
@@ -55,7 +55,7 @@ bool Waypoint::reactOnUse(int id)
 	{
 		Player* pl;
 		pl = dynamic_cast<Player*>(World::getWorld()->getPlayer(id));
-		
+
 		pl->setUsingWaypoint(true);
 	}
 	return true;
