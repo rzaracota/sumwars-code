0df2ee5859a8a913d54ed43c0d8775742d6e2b21
wuha <>
2009-08-27 16:25:16 +0000

    einige nuetzliche debugfunktionen

--- data/lua/debug.lua
+++ data/lua/debug.lua
@@ -3,3 +3,15 @@ function printlist(tab)
 		print(key,value);
 	end;
 end;
+		
+function tt()
+	teleportPlayer(4,"aisMounPath","entry_east")
+end;
+		
+function xx(layer)
+				test = createScriptObject("test","goblin","circle","entry_north",0.5,layer);
+end;
+		
+function cc()
+	setScriptObjectAnimation(test,"walk",1000,"true");
+end;
