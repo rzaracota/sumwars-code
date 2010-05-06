1b33bdbf400d45f9cf9c75638d6f168eb08e3927
kemic <>
2010-05-06 21:38:37 +0000

    Uebersetzung aktualisiert

--- translation/update_translation.sh
+++ translation/update_translation.sh
@@ -6,23 +6,16 @@
 #msgmerge -v -s -U test_de.po test-new.pot
 
 echo "CREATE new potfiles"
-xgettext -d sumwars -o potfiles/sumwars-sumwars-new.pot ../src/core/*.cpp ../src/core/*.h ../src/gui/*.cpp ../src/gui/*.h
+xgettext -d sumwars -o potfiles/sumwars-sumwars.pot ../src/core/*.cpp ../src/core/*.h ../src/gui/*.cpp ../src/gui/*.h
 ../../tests/xmlgettext/xmlgettext ../data/
-xgettext --from-code utf-8 --language=Python -d events -o potfiles/sumwars-events-new.pot ../data/npc/*.xml ../data/quests/*.xml ../data/world/*.xml
-
-# Ist ein mergen hier ueberhaupt sinnvoll? Oder sollte direkt die neu erstellte pot mit der alten po gemergt werden.
-echo "MERGE new pots with pots"
-msgmerge -v -U potfiles/sumwars-sumwars.pot potfiles/sumwars-sumwars-new.pot
-msgmerge -v -U potfiles/sumwars-xml.pot potfiles/sumwars-xml-new.pot
-msgmerge -v -U potfiles/sumwars-events.pot potfiles/sumwars-events-new.pot
+xgettext --from-code utf-8 --language=Python -d events -o potfiles/sumwars-events.pot ../data/npc/*.xml ../data/quests/*.xml ../data/world/*.xml
 
 echo "MERGE new pots with pos"
-msgmerge -v -U de/sumwars-sumwars.po potfiles/sumwars-sumwars-new.pot
-msgmerge -v -U de/sumwars-xml.po potfiles/sumwars-xml-new.pot
-msgmerge -v -U de/sumwars-events.po potfiles/sumwars-events-new.pot
+#msgmerge -v -U de/sumwars-sumwars.po potfiles/sumwars-sumwars.pot
+#msgmerge -v -U de/sumwars-xml.po potfiles/sumwars-xml.pot
+#msgmerge -v -U de/sumwars-events.po potfiles/sumwars-events.pot
 
-echo "REMOVE temporary new pots"
-rm potfiles/sumwars-sumwars-new.pot
-rm potfiles/sumwars-xml-new.pot
-rm potfiles/sumwars-events-new.pot
+msgmerge -v -U en/sumwars-sumwars.po potfiles/sumwars-sumwars.pot
+msgmerge -v -U en/sumwars-xml.po potfiles/sumwars-xml.pot
+msgmerge -v -U en/sumwars-events.po potfiles/sumwars-events.pot
 
