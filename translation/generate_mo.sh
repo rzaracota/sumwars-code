41c6b747efcf96dc2b80f45362b67a500a6bb87a
kemic <>
2010-05-20 20:18:27 +0000

    translation reorganization finished

--- translation/generate_mo.sh
+++ translation/generate_mo.sh
@@ -2,8 +2,8 @@
 
 # German
 echo "de mo"
-#msgfmt -c -v -o de/LC_MESSAGES/sumwars-sumwars.mo de/sumwars-sumwars.po
-#msgfmt -c -v -o de/LC_MESSAGES/sumwars-xml.mo de/sumwars-xml.po
+msgfmt -c -v -o de/LC_MESSAGES/sumwars-sumwars.mo de/sumwars-sumwars.po
+msgfmt -c -v -o de/LC_MESSAGES/sumwars-xml.mo de/sumwars-xml.po
 msgfmt -c -v -o de/LC_MESSAGES/sumwars-events.mo de/sumwars-events.po
 
 # English
