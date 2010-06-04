f636217d1d52825f0ba66bae472cb0c67b773aa4
wuha <>
2010-06-04 22:41:31 +0000

    updated contributor list

--- src/gui/creditswindow.cpp
+++ src/gui/creditswindow.cpp
@@ -21,15 +21,18 @@ CreditsWindow::CreditsWindow(Document* doc)
 	pane->setPosition(CEGUI::UVector2(cegui_reldim(0.0f), cegui_reldim(0.0f)));
 	pane->setSize(CEGUI::UVector2(cegui_reldim(1.0f), cegui_reldim( 1.0f)));
 	pane->setInheritsAlpha(false);
+	pane->setContentPaneAutoSized(true);
 	
 	CEGUI::Window* credits;
 	credits = win_mgr.createWindow("TaharezLook/StaticText", "CreditWindow");
 	pane->addChildWindow(credits);
-	credits->setPosition(CEGUI::UVector2(cegui_reldim(0.0f), cegui_reldim( 0.0f)));
+	credits->setPosition(CEGUI::UVector2(cegui_reldim(0.0f), cegui_reldim( 0.00f)));
 	credits->setSize(CEGUI::UVector2(cegui_reldim(1.00f), cegui_reldim( 1.0f)));
 	credits->setProperty("FrameEnabled", "false");
 	credits->setProperty("BackgroundEnabled", "true");
 	credits->setProperty("HorzFormatting", "HorzCentred");
+	credits->setMaxSize(CEGUI::UVector2(cegui_reldim(1.00f), cegui_reldim(10.0f)));
+	
 	std::string content = "Programming: \
 			\n Hans Wulf (Lastmerlin) \
 			\n fusion44 \
@@ -38,12 +41,16 @@ CreditsWindow::CreditsWindow(Document* doc)
 			\n Andreas Schönefeldt \
 			\n Steffen Schönefeldt (Nori) \
 			\n psycho \
+			\n Francesco Miglietta (hal9000 ) \
 			\n\n Scripting: \
 			\n Steffen Schönefeldt (Nori) \
 			\n Hans Wulf (Lastmerlin) \
 			\n\nStory: \
 			\n Steffen Schönefeldt (Nori) \
 			\n Andreas Schönefeldt \
+			\n\nSounds: \
+			\n Michael Kempf (Hangman) \
+			\n artisticdude \
 			\n\n Translation: \
 			\n Steffen Schönefeldt (Nori) \
 			\n Michael Kempf (Hangman) \
@@ -56,6 +63,7 @@ CreditsWindow::CreditsWindow(Document* doc)
 			\n\nWebsite: \
 			\n Andreas Schönefeldt \
 			\n Michael Kempf (Hangman)";
+	
 	credits->setText((CEGUI::utf8*) content.c_str());
 	credits->setProperty("BackgroundColours", "tl:99000000 tr:99000000 bl:99000000 br:99000000");
 	credits->setAlpha(0.9);
@@ -70,6 +78,7 @@ void CreditsWindow::updateTranslation()
 {
 	CEGUI::WindowManager& win_mgr = CEGUI::WindowManager::getSingleton();
 	CEGUI::Window* wtext = win_mgr.getWindow("CreditWindow");
+	
 	
 	CEGUI::Font* fnt = wtext->getFont();
 	
@@ -94,6 +103,9 @@ void CreditsWindow::update()
 		float starttime = 2000;	// time before scrolling starts
 		float alltime = 20000;	// time for scrolling
 		float pos = (m_shown_timer.getTime()-starttime)/alltime;
+		
+		if (pos < 0)
+			pos = 0;
 		
 		if (pos > 1.0)
 			pos = 1.0;
