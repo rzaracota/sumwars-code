d822bf266667a8f3ef680f4cfebb2045e474a9ff
kemic <>
2010-06-15 14:27:44 +0000

    new languages added

--- translation/generate_mo.sh
+++ translation/generate_mo.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# German
-echo "de mo"
+echo "German sumwars xml events"
 msgfmt -c -v -o de/LC_MESSAGES/sumwars-sumwars.mo de/sumwars-sumwars.po
 msgfmt -c -v -o de/LC_MESSAGES/sumwars-xml.mo de/sumwars-xml.po
 msgfmt -c -v -o de/LC_MESSAGES/sumwars-events.mo de/sumwars-events.po
 
+echo "Italian sumwars xml events"
+msgfmt -c -v -o it/LC_MESSAGES/sumwars-sumwars.mo de/sumwars-sumwars.po
+msgfmt -c -v -o it/LC_MESSAGES/sumwars-xml.mo de/sumwars-xml.po
+msgfmt -c -v -o it/LC_MESSAGES/sumwars-events.mo de/sumwars-events.po
+
+echo "Polish sumwars xml events"
+msgfmt -c -v -o pl/LC_MESSAGES/sumwars-sumwars.mo de/sumwars-sumwars.po
+msgfmt -c -v -o pl/LC_MESSAGES/sumwars-xml.mo de/sumwars-xml.po
+msgfmt -c -v -o pl/LC_MESSAGES/sumwars-events.mo de/sumwars-events.po
+
 # English
-echo "en mo"
-msgfmt -c -v -o en/LC_MESSAGES/sumwars-sumwars.mo en/sumwars-sumwars.po
-msgfmt -c -v -o en/LC_MESSAGES/sumwars-xml.mo en/sumwars-xml.po
+#echo "en mo"
+#msgfmt -c -v -o en/LC_MESSAGES/sumwars-sumwars.mo en/sumwars-sumwars.po
+#msgfmt -c -v -o en/LC_MESSAGES/sumwars-xml.mo en/sumwars-xml.po
 #msgfmt -c -v -o en/LC_MESSAGES/sumwars-events.mo en/sumwars-events.po
 
