4aa9fb40b999640480dd858f608cd71b4a3fd645
ig0rk0 <>
2011-09-29 17:28:37 +0000

    make shure we are getting the same results when updating translations under Windows and Linux

--- translation/update_translation.sh
+++ translation/update_translation.sh
@@ -5,53 +5,48 @@
 
 echo "Update translations ..."
 
-# copy newest xmlgettext version
-cp ./xmlgettext/xmlgettext ./potfiles/
+echo Be shure you have installed dos2unix package
 
-
-echo -n "Create new potfiles"
-xgettext -d menu -o potfiles/menu.pot ../src/core/*.cpp ../src/core/*.h ../src/gui/*.cpp ../src/gui/*.h
-cd potfiles ; ./xmlgettext ../../data/ ; cd ..
-#../../tests/xmlgettext/xmlgettext ../data/
-#xgettext --from-code utf-8 --language=Python -d events -o potfiles/sumwars-events.pot ../data/npc/*.xml ../data/quests/*.xml ../data/world/*.xml
-
-
-ls potfiles/*.pot | sed "s/.pot//;s/potfiles\///" > domains
+read -p "Press enter to continue" nothing
 
 while read LINE; do
 	LANGUAGE="de"
 	echo "+++ "$LINE" - GERMAN +++"
 	echo "[ PO ]"
-	msgmerge -v -U $LANGUAGE/$LINE.po potfiles/$LINE.pot
+	msgmerge -v -U $LANGUAGE/$LINE.po en/$LINE.pot
+	unix2dos $LANGUAGE/$LINE.po
 	echo "[ MO ]"
 	msgfmt -c -v -o $LANGUAGE/LC_MESSAGES/$LINE.mo $LANGUAGE/$LINE.po
 
 	LANGUAGE="it"
 	echo "+++ "$LINE" - ITALIAN +++"
 	echo "[ PO ]"
-	msgmerge -v -U $LANGUAGE/$LINE.po potfiles/$LINE.pot
+	msgmerge -v -U $LANGUAGE/$LINE.po en/$LINE.pot
+	unix2dos $LANGUAGE/$LINE.po
 	echo "[ MO ]"
 	msgfmt -c -v -o $LANGUAGE/LC_MESSAGES/$LINE.mo $LANGUAGE/$LINE.po
 
 	LANGUAGE="ru"
 	echo "+++ "$LINE" - RUSSIAN +++"
 	echo "[ PO ]"
-	msgmerge -v -U $LANGUAGE/$LINE.po potfiles/$LINE.pot
+	msgmerge -v -U $LANGUAGE/$LINE.po en/$LINE.pot
+	unix2dos $LANGUAGE/$LINE.po
 	echo "[ MO ]"
 	msgfmt -c -v -o $LANGUAGE/LC_MESSAGES/$LINE.mo $LANGUAGE/$LINE.po
-
 
 	LANGUAGE="pt"
 	echo "+++ "$LINE" - PORTUGUESE +++"
 	echo "[ PO ]"
-	msgmerge -v -U $LANGUAGE/$LINE.po potfiles/$LINE.pot
+	msgmerge -v -U $LANGUAGE/$LINE.po en/$LINE.pot
+	unix2dos $LANGUAGE/$LINE.po
 	echo "[ MO ]"
 	msgfmt -c -v -o $LANGUAGE/LC_MESSAGES/$LINE.mo $LANGUAGE/$LINE.po
 
 	LANGUAGE="uk"
 	echo "+++ "$LINE" - UKRAINIAN +++"
 	echo "[ PO ]"
-	msgmerge -v -U $LANGUAGE/$LINE.po potfiles/$LINE.pot
+	msgmerge -v -U $LANGUAGE/$LINE.po en/$LINE.pot
+	unix2dos $LANGUAGE/$LINE.po
 	echo "[ MO ]"
 	msgfmt -c -v -o $LANGUAGE/LC_MESSAGES/$LINE.mo $LANGUAGE/$LINE.po
 done < domains
