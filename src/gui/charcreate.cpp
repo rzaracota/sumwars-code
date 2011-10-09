b2b553374604f544c0c823c93c29a7d94d0dad8b
ig0rk0 <>
2011-10-09 15:13:24 +0000

    move Create Character menu to layout file

--- src/gui/charcreate.cpp
+++ src/gui/charcreate.cpp
@@ -31,68 +31,30 @@ CharCreate::CharCreate (Document* doc)
 	
 
 	// Rahmen fuer das Menue Savegame auswaehlen
-	CEGUI::FrameWindow* char_create = (CEGUI::FrameWindow*) win_mgr.createWindow("TaharezLook/FrameWindow", "CharCreate");
+	CEGUI::FrameWindow* char_create = (CEGUI::FrameWindow*) win_mgr.loadWindowLayout("CharCreate.layout");
 	m_window = char_create;
-	
-	char_create->setPosition(CEGUI::UVector2(cegui_reldim(0.0f), cegui_reldim( 0.0f))); //0.0/0.8
-	char_create->setSize(CEGUI::UVector2(cegui_reldim(0.5f), cegui_reldim( 1.0f))); //1.0/0.2
-	char_create->setProperty("FrameEnabled","false");
-	char_create->setProperty("TitlebarEnabled","false");
-	char_create->setProperty("CloseButtonEnabled","false");
 	
 	// Bestandteile der Kontrollleiste hinzufuegen
 	CEGUI::PushButton* btn;
-	CEGUI::Window* label;
-
-	// Label Name
-	label = win_mgr.createWindow("TaharezLook/StaticText", "CharNameLabel");
-	char_create->addChildWindow(label);
-	label->setProperty("FrameEnabled", "true");
-	label->setProperty("BackgroundEnabled", "true");
-	label->setPosition(CEGUI::UVector2(cegui_reldim(0.05f), cegui_reldim( 0.05f)));
-	label->setSize(CEGUI::UVector2(cegui_reldim(0.2f), cegui_reldim( 0.05f)));
-	
 	CEGUI::Editbox* namebox;
 	
-	namebox = static_cast<CEGUI::Editbox*>(win_mgr.createWindow("TaharezLook/Editbox", "NameBox"));
-	char_create->addChildWindow(namebox);
-	namebox->setPosition(CEGUI::UVector2(cegui_reldim(0.3f), cegui_reldim( 0.05f)));
-	namebox->setSize(CEGUI::UVector2(cegui_reldim(0.6f), cegui_reldim( 0.05f)));
+	namebox = static_cast<CEGUI::Editbox*>(win_mgr.getWindow("NameBox"));
 	namebox->setWantsMultiClickEvents(false);
 	namebox->setMaxTextLength(31);
 	
-	label = win_mgr.createWindow("TaharezLook/StaticText", "CharClassLabel");
-	char_create->addChildWindow(label);
-	label->setProperty("FrameEnabled", "true");
-	label->setProperty("BackgroundEnabled", "true");
-	label->setPosition(CEGUI::UVector2(cegui_reldim(0.05f), cegui_reldim( 0.12f)));
-	label->setSize(CEGUI::UVector2(cegui_reldim(0.2f), cegui_reldim( 0.05f)));
-	
-	CEGUI::Listbox* classlist = (CEGUI::Listbox*) win_mgr.createWindow("TaharezLook/Listbox", "ClassList");
-	char_create->addChildWindow(classlist);
-	classlist->setPosition(CEGUI::UVector2(cegui_reldim(0.05f), cegui_reldim( 0.2f)));
-	classlist->setSize(CEGUI::UVector2(cegui_reldim(0.8f), cegui_reldim( 0.2f)));
+	CEGUI::Listbox* classlist = (CEGUI::Listbox*) win_mgr.getWindow("ClassList");
 	classlist->subscribeEvent(CEGUI::Listbox::EventSelectionChanged, CEGUI::Event::Subscriber(&CharCreate::onClassSelected, this));
 
-	CEGUI::Listbox* looklist = (CEGUI::Listbox*) win_mgr.createWindow("TaharezLook/Listbox", "LookList");
-	char_create->addChildWindow(looklist);
-	looklist->setPosition(CEGUI::UVector2(cegui_reldim(0.05f), cegui_reldim( 0.42f)));
-	looklist->setSize(CEGUI::UVector2(cegui_reldim(0.8f), cegui_reldim( 0.45f)));
+	CEGUI::Listbox* looklist = (CEGUI::Listbox*) win_mgr.getWindow("LookList");
 	looklist->subscribeEvent(CEGUI::Listbox::EventSelectionChanged, CEGUI::Event::Subscriber(&CharCreate::onLookSelected, this));
 
 	// Button Savegame akzeptieren
-	btn = static_cast<CEGUI::PushButton*>(win_mgr.createWindow("TaharezLook/Button", "AbortButton"));
-	char_create->addChildWindow(btn);
-	btn->setPosition(CEGUI::UVector2(cegui_reldim(0.1f), cegui_reldim( 0.9f)));
-	btn->setSize(CEGUI::UVector2(cegui_reldim(0.2f), cegui_reldim( 0.05f)));
+	btn = static_cast<CEGUI::PushButton*>(win_mgr.getWindow("AbortButton"));
 	btn->subscribeEvent(CEGUI::PushButton::EventClicked, CEGUI::Event::Subscriber(&CharCreate::onButtonAbort, this));
 	btn->setWantsMultiClickEvents(false);
 	
 	// Button neu
-	btn = static_cast<CEGUI::PushButton*>(win_mgr.createWindow("TaharezLook/Button", "CharCreateButton"));
-	char_create->addChildWindow(btn);
-	btn->setPosition(CEGUI::UVector2(cegui_reldim(0.35f), cegui_reldim( 0.9f)));
-	btn->setSize(CEGUI::UVector2(cegui_reldim(0.6f), cegui_reldim( 0.05f)));
+	btn = static_cast<CEGUI::PushButton*>(win_mgr.getWindow("CharCreateButton"));
 	btn->subscribeEvent(CEGUI::PushButton::EventClicked, CEGUI::Event::Subscriber(&CharCreate::onButtonCharCreate, this));
 	btn->setWantsMultiClickEvents(false);
 	
