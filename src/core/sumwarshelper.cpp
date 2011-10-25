1875944141fc6924ad124de2f7584f7e31be5cf0
thegusty999 <>
2011-10-25 14:00:19 +0000

    New helper function. (used in string parsing for used screen resolution)

---
+++ src/core/sumwarshelper.cpp
@@ -0,0 +1,56 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sumwarshelper.h"
+
+
+
+std::string SumwarsHelper::getUpdatedResolutionString (const std::string& initialString, int newWidth, int newHeight)
+{
+	std::string returnValue;
+	returnValue = initialString;
+
+	std::string sLeft, sRight;
+	int nPos = returnValue.find (" ");
+	if (nPos == std::string::npos)
+	{
+		// some error...
+		return "";
+	}
+
+	sLeft = returnValue.substr (0, nPos);
+	sRight= returnValue.substr (nPos + 3); // + 3 chars for " x "
+	nPos = sRight.find (" ");
+	std::string sAux;
+
+	if (nPos == std::string::npos)
+	{
+		// we don't have a colour depth.
+		sAux = sRight;
+		sRight = "";
+	}
+	else
+	{
+		sAux = sRight.substr(0, nPos);
+		sRight = sRight.substr(nPos);
+	}
+
+	// Use a string stream to parse the needed string.
+	std::stringstream ss;
+	ss << newWidth << " x " << newHeight << sRight;
+	returnValue = ss.str ();
+
+	return returnValue;
+}
--- src/core/sumwarshelper.h
+++ src/core/sumwarshelper.h
@@ -20,6 +20,9 @@
 #include "OgreString.h"
 // Return the path to where files should be stored on mac
 
+/**
+ * \brief Helper class. Used as a namespace to store utility static functions.
+ */
 class SumwarsHelper
 {
 	/**
@@ -37,5 +40,41 @@ public:
 #endif
 		return path;
 	}
+
+
+	/**
+	 * \brief Gets the updated resolution string from a base resolution string.
+	 * Purpose: manage differences between Direct3D and OpenGL resolution strings.
+	 * 
+	 * \param initialString The source resolution string.
+	 * \param newWidth The new width to use.
+	 * \param newHeight The new height to use.
+	 * \return The full updated name of the entry for the resolution.
+	 * \author Augustin Preda (if anything doesn't work, he's the one to bash).
+	 */
+	static std::string getUpdatedResolutionString (const std::string& initialString, int newWidth, int newHeight);
+
+
+#ifdef __APPLE__
+	// TODO: copied function here from application.h; Investigate integration with userPath function.
+	// TODO: investigate removal of original function from application.h
+
+    /**
+     * \fn Ogre::String macPath()
+     * \brief Returns the path to the Resources directory on mac.
+     */
+    Ogre::String macPath()
+    {
+        Ogre::String path;
+        CFBundleRef mainBundle = CFBundleGetMainBundle();
+        CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+        char resPath[PATH_MAX];
+        CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)resPath, PATH_MAX);
+        CFRelease(resourcesURL);
+        path = resPath;
+        return path;
+    }
+#endif
+
 };
 #endif // SUMWARSHELPER_H
