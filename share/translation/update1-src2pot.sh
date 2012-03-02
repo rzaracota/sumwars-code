45359640279020ee850568836f3973c7ed391dc1
Igor Paliychuk <mansonigor@gmail.com>
2012-03-03 00:35:30 +0200

    Don't do shit

--- share/translation/update1-src2pot.sh
+++ share/translation/update1-src2pot.sh
@@ -6,7 +6,7 @@ cp ./xmlgettext/xmlgettext ./potfiles/
 
 echo "Creating new potfiles..."
 xgettext -d menu -o potfiles/menu.pot ../../src/core/*.cpp ../../src/core/*.h ../../src/gui/*.cpp ../../src/gui/*.h
-xgettext -d tools -o potfiles/tools.pot ../src/gui/contenteditor/*.cpp ../src/gui/contenteditor/*.h ../src/gui/debugpanel/*.cpp ../src/gui/debugpanel/*.h ../src/gui/debugpanel/filebrowser/*.cpp ../src/gui/debugpanel/filebrowser/*.h
+xgettext -d tools -o potfiles/tools.pot ../../src/gui/contenteditor/*.cpp ../../src/gui/contenteditor/*.h ../../src/gui/debugpanel/*.cpp ../../src/gui/debugpanel/*.h ../../src/gui/debugpanel/filebrowser/*.cpp ../../src/gui/debugpanel/filebrowser/*.h
 cd potfiles
 ./xmlgettext ../../data/
 cd ..
