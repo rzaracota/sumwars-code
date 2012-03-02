871622055a009376882b57ca8fd20bdd5ab7aa2c
Michael Kunz (Protogenes) <Michael.Kunz@s2005.tu-chemnitz.de>
2012-03-02 16:58:20 +0100

    ability to specify on command-line which translations should be updated

--- share/translation/update2-pot2po.sh
+++ share/translation/update2-pot2po.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
 skipdirectories="potfiles xmlgettext"
-langs=$(ls -d */)
-pots=$(ls potfiles/*.pot)
-pots=${pots//potfiles\//}
+
+if [ -z "$*" ]; then
+	langs=$(ls -d */)
+	langs=${langs//\//}
+else
+	for l in $*
+	do
+		if [ -d "$l" ]; then
+			langs="$langs $l"
+		else
+			echo "$l is unknown!"
+		fi
+	done
+fi
 
 for l in $skipdirectories
 do
-	langs=${langs//$l\//}
+	langs=${langs//$l/}
 done
+
+pots=$(ls potfiles/*.pot)
+pots=${pots//potfiles\//}
 
 for l in $langs
 do
 	for p in $pots
 	do
-		po=$l${p%t}
+		po=$l/${p%t}
 		# create the file if it doesn't exist
 		echo -ne "\E[1;31m$po \E[0m"
 		if [ -f "$po" ] ; then
 			msgmerge --color=auto --force-po --lang="$l" -F -U "$po" "potfiles/$p"
 		else
-			msginit --color=auto -l "${l%/}" -i "potfiles/$p" -o "$po"
+			msginit --color=auto -l "$l" -i "potfiles/$p" -o "$po"
 		fi
 		#unix2dos $po
 	done
--- share/translation/update3-po2mo.sh
+++ share/translation/update3-po2mo.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 skipdirectories="potfiles xmlgettext"
-langs=$(ls -d */)
+
+if [ -z "$*" ]; then
+	langs=$(ls -d */)
+	langs=${langs//\//}
+else
+	for l in $*
+	do
+		if [ -d "$l" ]; then
+			langs="$langs $l"
+		else
+			echo "$l is unknown!"
+		fi
+	done
+fi
 
 for l in $skipdirectories
 do
@@ -16,7 +29,7 @@ do
 	[ -d "$l/LC_MESSAGES" ] || mkdir "$l/LC_MESSAGES"
 	for p in $pos
 	do
-		echo -ne "\E[1;31m$l$p \E[0m"
+		echo -ne "\E[1;31m$l/$p \E[0m"
 		msgfmt -c -v -o "$l/LC_MESSAGES/${p/%po/mo}" "$l/$p"
 	done
 	echo
