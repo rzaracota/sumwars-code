0f52d2deadd52a34df2cbcd8d2236a8c13deced9
Martin Preisler <preisler.m@gmail.com>
2012-03-04 16:41:58 +0100

    Don't load Ogre's CgProgramManager plugin by default, it's not currently needed

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -188,8 +188,7 @@ bool Application::init()
 		"Plugin=RenderSystem_Direct3D9" SUMWARS_DEBUG_POSTFIX "\n"
 #endif
 		"Plugin=Plugin_ParticleFX" SUMWARS_DEBUG_POSTFIX "\n"
-		"Plugin=Plugin_OctreeSceneManager" SUMWARS_DEBUG_POSTFIX "\n"
-		"Plugin=Plugin_CgProgramManager" SUMWARS_DEBUG_POSTFIX "\n";
+		"Plugin=Plugin_OctreeSceneManager" SUMWARS_DEBUG_POSTFIX "\n";
 
 #include "default_resources.cfg.inc"
 
