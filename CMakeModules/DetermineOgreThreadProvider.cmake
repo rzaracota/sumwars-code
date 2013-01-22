3f201909d68e753100e85d72ee25d3a450f2acf6
Augustin Preda (dezGusty) <>
2013-01-22 19:42:31 +0200

    Added header to script DetermineOgreThreadProvider.cmake

--- CMakeModules/DetermineOgreThreadProvider.cmake
+++ CMakeModules/DetermineOgreThreadProvider.cmake
@@ -1,4 +1,42 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# This file is part of the CMake build system for Summoning Wars (http://sumwars.org/)
+#
+# The contents of this file are placed in the public domain. Feel free to make use of it in any way you like.
+# ----------------------------------------------------------------------------------------------------------------------
+# 
+# Determine the library that is used to provide threading support in OGRE.
+# This script provides a set of functions to access the configured OGRE installation and read the OgreBuildSettings file.
+# 
+# E.g.: To store the ogre thread provider into the variable "OGRE_THREAD_PROVIDER", use the function call:
+#   DETERMINE_OGRE_THREAD_PROVIDER(OGRE_THREAD_PROVIDER ${MY_OGRE_FOLDER}/OgreBuildSettings.h)
+#
+# Author: Augustin Preda.
+#     Based on the "DetermineVersion.cmake" file by  Reto Grieder.
+# Revision history:
+# version 1, 2012.01.13: First commit of file. Single function: DETERMINE_OGRE_THREAD_PROVIDER
+# 
+# ----------------------------------------------------------------------------------------------------------------------
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Functions:
+# - DETERMINE_OGRE_THREAD_PROVIDER
+# 
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Determine the OGRE Thread provider configured in the header file.
+# Parameters:
+#   - _tp_var: Output parameter. The variable in which to store the OGRE_THREAD_PROVIDER read from the OGRE config.
+#   - _file: The name and path of the file to search into. This should typically be the "OgreBuildSettings.h" file.
+#
+# At the time of the writing of this function, the following possible values are defined in OGRE and can be stored in 
+#_tp_var:
+#   1 = boost
+#   2 = poco
+#   3 = tbb
+# ----------------------------------------------------------------------------------------------------------------------
 FUNCTION(DETERMINE_OGRE_THREAD_PROVIDER _tp_var _file)
   IF(EXISTS ${_file})
     FILE(READ ${_file} _file_content)
